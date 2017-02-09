4aa76509b8ab2c01faa3f4c6306f1e63ed4fc8bd
Lewis-Kelley <kelleyld@rose-hulman.edu>
2017-02-09 12:16:39 -0500

    Improved the searching SPROC's.

--- sql-queries/searchGames.sql
+++ sql-queries/searchGames.sql
@@ -3,6 +3,18 @@ CREATE PROCEDURE searchGames
 AS
 -- String splitting logic taken from:
 -- http://stackoverflow.com/questions/2647/how-do-i-split-a-string-so-i-can-access-item-x#2685
-SET @text = REPLACE(@text, ' ', ' OR ')
-SELECT * FROM Game
-  WHERE CONTAINS ((GName, Series, Studio), @text)
+DECLARE @andText VARCHAR(255)
+DECLARE @orText VARCHAR(255)
+SET @andText = REPLACE(@text, ' ', ' AND ')
+SET @orText = REPLACE(@text, ' ', ' OR ')
+SELECT *
+  FROM Game
+  WHERE CONTAINS((GName, Series, Studio), @andText)
+        OR CONTAINS((GName, Series, Studio), @orText)
+  -- Sort so that those that have all the keywords are shown before
+  -- those with only some.
+  ORDER BY (CASE WHEN CONTAINS((GName, Series, Studio), @andText)
+                   THEN 1
+                 WHEN CONTAINS((GName, Series, Studio), @orText)
+                   THEN 2
+                 END)
--- sql-queries/searchMods.sql
+++ sql-queries/searchMods.sql
@@ -3,6 +3,18 @@ CREATE PROCEDURE searchMods
 AS
 -- String splitting logic taken from:
 -- http://stackoverflow.com/questions/2647/how-do-i-split-a-string-so-i-can-access-item-x#2685
-SET @text = REPLACE(@text, ' ', ' OR ')
-SELECT * FROM Mod
-  WHERE CONTAINS (MName, @text)
+DECLARE @andText VARCHAR(255)
+DECLARE @orText VARCHAR(255)
+SET @andText = REPLACE(@text, ' ', ' AND ')
+SET @orText = REPLACE(@text, ' ', ' OR ')
+SELECT *
+  FROM Mod
+  WHERE CONTAINS(MName, @andText)
+        OR CONTAINS(MName, @orText)
+  -- Sort so that those that have all the keywords are shown before
+  -- those with only some.
+  ORDER BY (CASE WHEN CONTAINS(MName, @andText)
+                   THEN 1
+                 WHEN CONTAINS(MName, @orText)
+                   THEN 2
+                 END)

00214f17816e36e35ef75dc94d4c9924bf7c891c
kelleyld <kelleyld@rose-hulman.edu>
2017-02-11 21:33:29 -0500

    Some work to get login 'working'

--- main.py
+++ main.py
@@ -5,22 +5,36 @@
 import pyodbc
 from flask import Flask, request, session, g, redirect, \
     url_for, abort, render_template, flash
-from flask_login import login_required
+from flask_login import LoginManager, login_required
 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=titan.csse.rose-hulman.edu;DATABASE=ReallyBigGameDatabase;UID=lix4;PWD=cjlxw1h,.')
 cursor=conn.cursor()
 cursor.execute("SELECT * FROM Game")
 rows = cursor.fetchall()
-for row in rows:
-    print row.GName
+# for row in rows:
+#   print row.GName
 
 app = Flask(__name__)
 
 
+
+# Holds all the user objects for all users (ever?)
+users = []
+
+# Used for login management. Takes in the unicode ID of the user and
+# return the object associated with it.
+# @login_manager.user_loader
+# def load_user(user_id):
+#     for user in users:
+#         if (user.get_id() == user_id):
+#             return user
+#     return None
+
+@login_required
 @app.route("/", methods = ['GET', 'POST'])
 def main():
     if (request.method == 'POST'):
-        return render_template('show_entries.html', entries=search()), showRecommendation()
+        return render_template('show_entries.html', entries=search())#, showRecommendation()
     elif (request.method == 'GET'):
         rows = showInfo()
         return render_template('show_entries.html', entries=rows, recommendations=rows)
@@ -30,22 +44,20 @@ def showInfo():
     rows = cursor.fetchall()
     return rows
 
-@app.route("/login", methods=['GET'])
-@login_required
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
-    print("should print sth")
-    # print(request.form['userName'])
-    # print(request.form['passWord'])
     # cursor.execute("EXEC login " + request.form['userName'] + request.form['passWord'])
     # result = cursor.fetchall()
     # if (result == True):
-    #     pass
+    #     print("Yay!")
     # else:
-    #     pass
+    #     print("Aww...")
     return render_template('login.html')
 
+# TODO Help me I'm broken!
 def showRecommendation():
-    cursor.execute()
+    # The hardcoded arguments are just for testing
+    cursor.execute("DECLARE @results TABLE(g_id int NULL, m_id int NULL); EXEC recommendations 'kelleyld', 10, 1770; SELECT * FROM @results")
     return cursor.fetchall()
 
 def search():
@@ -54,4 +66,8 @@ def search():
        return cursor.fetchall()
 
 if __name__ == "__main__":
+    app.secret_key = 'MySuperSecretPassword'
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
     app.run()
